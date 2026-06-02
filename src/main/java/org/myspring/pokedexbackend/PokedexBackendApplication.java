d091876c74413668d1cfb9fb5c0202da0a2e18ea
faisalprady13 <faisalprady@gmail.com>
2026-06-02 09:57:14 +0200

    initial commit

---
+++ src/main/java/org/myspring/pokedexbackend/PokedexBackendApplication.java
@@ -0,0 +1,13 @@
+package org.myspring.pokedexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/myspring/pokedexbackend/PokedexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myspring.pokedexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
